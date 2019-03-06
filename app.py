6e20a00f656c3a53e10ebd793e2496049e826ca3
bella <564414779@qq.com>
2019-03-06 19:38:45 +0000

    add4
    add4

--- app.py
+++ app.py
@@ -1,35 +1,29 @@
-from flask import Flask, render_template
-
+#from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def say_hello():
   return render_template("index.html")
-
+ 
 @app.route("/<name>")
 def say_name(name):
   # return "hello {}".format(name)
-  return f"hello {name}"
-
+  #return f"hello {name}"
+  return render_template("index.html", user=name)
   
-@app.route("/JKF")
-def say_hello1():
-  return render_template("index1.html")
+#@app.route("/JKF")
+#def say_hello1():
+ # return render_template("index1.html")
 
-@app.route("/JKF/AAA")
-def say_hello2():
-  return render_template("index.html")
+#@app.route("/JKF/AAA")
+#def say_hello2():
+ # return render_template("index.html")
+@app.route("/feedback", methods=["POST"])
+def get_feedback():
+ 
+  data = request.values
 
+  return render_template("feedback.html", form_data=data)
 app.run(debug=True)
 
-nav ul {
-  list-style: none;
-  font-family: Arial, sans-serif;
-  font-size: 20px;
-  text-align: center;
-}
-
-nav ul li {
-  display: inline;
-  margin-right: 20px;
-}
