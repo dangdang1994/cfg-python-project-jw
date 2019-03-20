798840dd0754e61aed7ade37f26b1a7cd554424d
bella <564414779@qq.com>
2019-03-20 20:01:11 +0000

    last
    last

--- app.py
+++ app.py
@@ -19,11 +19,12 @@ def say_name(name):
 #@app.route("/JKF/AAA")
 #def say_hello2():
  # return render_template("index.html")
-@app.route("/feedback", methods=["POST"])
+@app.route("/feedback", methods=["POST"])#若不加post所输入的内容将显示在IP地址中
 def get_feedback():
  
   data = request.values
 
   return render_template("feedback.html", form_data=data)
+  
 app.run(debug=True)
 
